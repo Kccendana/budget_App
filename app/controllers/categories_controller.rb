ab7e387d41329c115699f3e81cb42b56c863214c
kcccendana <kc_telyn@yahoo.com>
2023-11-30 19:56:37 +0900

    Create categories_controller.rb

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,58 @@
+class CategoriesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_category, only: %i[show edit update destroy]
+
+  def index
+    @categories = Category.includes(:payments).where(user_id: current_user.id).order(updated_at: :desc)
+  end
+
+  def show
+    redirect_to new_category_payment_path(@category)
+  end
+
+  def new
+    @category = current_user.categories.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    @category.user = current_user
+
+    respond_to do |format|
+      if @category.save
+        format.html { redirect_to categories_path, notice: 'Category was successfully created.' }
+        format.json { render :show, status: :created, location: @category }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @category.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @category.update(category_params)
+        format.html { redirect_to category_url(@category), notice: 'Category was successfully updated.' }
+        format.json { render :show, status: :ok, location: @category }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @category.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @category.destroy
+    redirect_to categories_url, notice: 'Category was successfully destroyed.'
+  end
+
+  private
+
+  def set_category
+    @category = Category.find(params[:id])
+  end
+
+  def category_params
+    params.require(:category).permit(:name, :icon)
+  end
+end
